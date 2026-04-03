655b47bf68d514148c814534753d710dda180260
aliuhysni-svg <aliu.hysni@hotmail.com>
2026-04-03 21:58:24 +0300

    Add CORS

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "https://floarko.fi",
+    "https://www.floarko.fi",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
     return {"message": "Forensic ADHD Backend Running"}
-
